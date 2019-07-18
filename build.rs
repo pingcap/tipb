eb506b78ee96d3cd10b41c57aca298a3e14e23a0
Jay Lee <busyjaylee@gmail.com>
2019-07-18 19:42:28 +0800

    support rust-protobuf by default
    and optionally support prost.
    
    Note that prost is broken currently, which should be fixed
    later.

--- build.rs
+++ build.rs
@@ -11,25 +11,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[cfg(feature = "gen")]
-use std::fs::File;
-#[cfg(feature = "gen")]
-use std::io::Write;
+use std::fs;
 
 fn main() {
-    if !cfg!(feature = "gen") {
-        println!("cargo:rerun-if-changed=build.rs");
-        return;
-    }
-
-    #[cfg(feature = "gen")]
-    {
-        use protobuf_build::*;
-        use std::fs;
-
-        check_protoc_version();
-
-        let mut mods: Vec<_> = fs::read_dir("proto")
+    let out_dir = format!("{}/protos", std::env::var("OUT_DIR").unwrap());
+    let mods: Vec<_> = fs::read_dir("proto")
             .expect("Couldn't read directory")
             .filter_map(|e| {
                 let file_name = e.expect("Couldn't list file").file_name();
@@ -37,40 +23,8 @@ fn main() {
                 if !file_name.ends_with(".proto") {
                     return None;
                 }
-                Some(file_name[..file_name.len() - 6].to_owned())
+                Some(format!("proto/{}", file_name))
             })
             .collect();
-        mods.sort();
-        let protos: Vec<_> = mods.iter().map(|m| format!("proto/{}.proto", m)).collect();
-
-        for p in &protos {
-            println!("cargo:rerun-if-changed={}", p);
-        }
-
-        // Rust-protobuf
-        generate_protobuf_files(&protos, "src/protobuf");
-        replace_read_unknown_fields(&protos);
-        generate_lib_file(&mods);
-
-        // Prost
-        generate_prost_files(&protos, "src/prost");
-        generate_wrappers(&["src/prost/tipb.rs"], "src/prost", GenOpt::all());
-        fs::remove_file("src/prost/gogoproto.rs").unwrap();
-        fs::remove_file("src/prost/google.protobuf.rs").unwrap();
-    }
-}
-
-#[cfg(feature = "gen")]
-fn generate_lib_file<T: AsRef<str>>(mod_names: &[T]) {
-    let mut text = String::new();
-
-    for mod_name in mod_names {
-        text.push_str("#[rustfmt::skip]\npub mod ");
-        text.push_str(mod_name.as_ref());
-        text.push_str(";\n");
-    }
-
-    let mut lib = File::create("src/protobuf.rs").expect("Could not create protobuf.rs");
-    lib.write_all(text.as_bytes())
-        .expect("Could not write protobuf.rs");
+    protobuf_build::generate_files(&["include".to_owned(), "proto".to_owned()], &mods, &out_dir);
 }
