25d390a064d25b65b668f70636bd759475c2186f
Nick Cameron <nrc@ncameron.org>
2019-08-02 19:21:23 +1200

    Use proto-build builder API
    Signed-off-by: Nick Cameron <nrc@ncameron.org>

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,9 @@
 #[allow(clippy::all)]
 #[allow(renamed_and_removed_lints)]
 #[allow(bare_trait_objects)]
+
 mod protos {
     include!(concat!(env!("OUT_DIR"), "/protos/mod.rs"));
 }
 
-pub use crate::protos::*;
+pub use crate::protos::tipb::*;
